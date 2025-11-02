157f19be62e7feb8ece9f3622434eeb043cd7276
nicolasgarcia06 <805@alumnos.fpsumma.es>
2025-11-02 19:07:30 +0100

    Update LoopMaster.java

--- LoopMaster.java
+++ LoopMaster.java
@@ -1,3 +1,19 @@
 public class LoopMaster {
-
+public static void contarHastaN(int n) {
+    for (int i = 1; i <= n; i++) {
+        System.out.println(i);
+    }
+}
+public static void imprimirSumaHastaN(int n) {
+    int suma = 0;
+    for (int i = 1; i <= n; i++) {
+        suma += i;
+        System.out.println("Suma hasta " + i + ": " + suma);
+    }
+}
+public static void imprimirTablaMultiplicar(int n) {
+    for (int i = 1; i <= 10; i++) {
+        System.out.println(n + " x " + i + " = " + (n * i));
+    }
+}
 }
