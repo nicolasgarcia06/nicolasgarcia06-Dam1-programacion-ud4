954aeecefe62c0cf4796d47cd028f4961b816736
nicolasgarcia06 <805@alumnos.fpsumma.es>
2025-11-18 08:20:57 +0100

    Update notasAlumnos.java

--- notasAlumnos.java
+++ notasAlumnos.java
@@ -1,9 +1,9 @@
 public class notasAlumnos {
     public static double[] CalcularMedias(int[][] notas){
-double [] notasAlumnos=new double(notas.length);
+double [] notasAlumnos=new int(notas.length);
 for(int i=0;i<notas.length;i++){
     double suma=0;
-fo
+
 }
     }
 }
